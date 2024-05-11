121c76c47a64c32124a9bf077930ea1db2dcc565
ribastudio <ribastudio@gmail.com>
2024-05-10 23:13:25 -0300

    impl: dark mode toggle

---
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import { Sun, Moon } from 'lucide-react';
+
+const DarkmodeToggle = () => {
+  const [currentTheme, setTheme] = useState(getInitialTheme());
+
+  useEffect(() => {
+    applyTheme();
+  }, [currentTheme]);
+
+  function getInitialTheme() {
+    let userTheme = null;
+    let systemTheme = true;
+    if (typeof window !== 'undefined' && window.localStorage) {
+      userTheme = localStorage.getItem('theme');
+      systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    }
+
+    return userTheme || (systemTheme ? 'dark' : 'light');
+  }
+
+  function applyTheme() {
+    if (currentTheme === 'dark') {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }
+
+  function toggleTheme() {
+    setTheme(currentTheme === 'dark' ? 'light' : 'dark');
+  }
+
+  return (
+    <>
+      {currentTheme === 'dark' ? (
+        <Sun width='1.875rem' height='1.875rem' onClick={toggleTheme} />
+      ) : (
+        <Moon width='1.875rem' height='1.875rem' onClick={toggleTheme} />
+      )}
+    </>
+  );
+}
+
+export { DarkmodeToggle };

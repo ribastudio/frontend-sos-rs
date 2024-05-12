d81960f6e34fa8696714af163108f848c6243a79
ribastudio <ribastudio@gmail.com>
2024-05-12 12:04:09 -0300

    fix: dark mode notFoundSearch component

--- src/components/NoFoundSearch/NoFoundSearch.tsx
+++ src/components/NoFoundSearch/NoFoundSearch.tsx
@@ -3,13 +3,13 @@ import { SearchX } from 'lucide-react';
 const NoFoundSearch = () => {
   return (
     <div className="flex flex-col justify-center items-center text-center p-8 gap-2">
-      <SearchX size={64} className="text-[#677183]" />
+      <SearchX size={64} className="text-[#677183] dark:stroke-[#aeaeae]" />
 
-      <h1 className="text-[#2f2f2f] font-semibold text-[18px]">
+      <h1 className="text-[#2f2f2f] dark:text-[#aeaeae] font-semibold text-[18px]">
         A sua busca não encontrou nenhum resultado{' '}
       </h1>
 
-      <h1 className="font-medium text-[#677183] text-[14px]">
+      <h1 className="font-medium text-[#677183] dark:text-[#f0f0f0]text-[14px]">
         Verifique os termos buscados e tente novamente
       </h1>
     </div>
